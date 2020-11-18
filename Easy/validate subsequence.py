14ba35be374259561cb7027f2353af41a951a772
AnimeshJha97 <AnimeshJha97@repo.github.com>
2020-11-18 21:04:41 +0530

    Update validate subsequence.py

--- Easy/validate subsequence.py
+++ Easy/validate subsequence.py
@@ -1,17 +1,21 @@
-arr = input().split()
-seq = input().split()
+#O(n) -> Time | O(1) -> Space
+# Steps :
+# 1) Initialize i and j as 0 where i will iterate through arr
+#    and j will iterate through seq
+# 2) If the first match is found, then only we increment i and j
+#    else we increment i as we move fwd in arr.
+# 3) c increments for every match. If c equals length of seq, print true.
+def validateSubsequence(arr, seq):
+    i = j = c = 0
+    while(i < len(arr) and j < len(seq)):
+        if (arr[i] == seq[j]) :
+            c += 1
+            i += 1
+            j += 1
+        else:
+            i += 1
 
-i = j = c = 0
-
-while(i < len(arr) and j < len(seq)):
-    if (arr[i] == seq[j]) :
-        c += 1
-        i += 1
-        j += 1
-    else:
-        i += 1
-
-if c == len(seq):
-    print('true')
-else  :
-    print('false')
+    if c == len(seq):
+        print('true')
+    else  :
+        print('false')
