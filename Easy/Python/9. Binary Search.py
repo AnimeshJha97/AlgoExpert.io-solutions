55c2479e09ee3e521d8d4b6576c526b67530a896
AnimeshJha97 <AnimeshJha97@repo.github.com>
2020-11-27 20:29:00 +0530

    Update 9. Binary Search.py

--- Easy/Python/9. Binary Search.py
+++ Easy/Python/9. Binary Search.py
@@ -1,8 +1,10 @@
 def binarySearch(ar, target):
-    return helper(ar, target, 0, len(ar))
+    return helper(ar, target, 0, len(ar)-1)
 
+# Recursive method
+# O(log n) -> Time | O(log n) -> Space
 def helper(ar, target, start, end):
-    mid = int((start + end) / 2)
+    mid = (start + end) // 2
     print("Start: " + str(start) + ",  End: " + str(end))
     if target is ar[mid]:
         return True
@@ -13,6 +15,20 @@ def helper(ar, target, start, end):
     else:
         return False
 
+# Iterative method
+# O(log n) -> Time | O(1) -> Space 
+def helper(ar, target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        if target is ar[mid]:
+            return True
+        elif target < ar[mid]:
+            end = mid-1
+        elif target > ar[mid]:
+            start = mid+1
+        else:
+            return False
+
 a = input()
 a = list(map(int, a.split(' ')))
 a.sort()
@@ -21,8 +37,3 @@ if(binarySearch(a, t)):
     print("Element Present in the array")
 else:
     print("Element Not Present in the array")
-
-
-
-
-    
